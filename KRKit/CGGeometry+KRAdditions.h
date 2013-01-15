f3a53937769b8abb95190ae9805449b6ddb54d0d
= <=>
2013-01-15 15:57:21 -0600

    added rect functions

---
+++ KRKit/CGGeometry+KRAdditions.h
@@ -0,0 +1,18 @@
+//
+//  CGGeometry+KRAdditions.h
+//  KRKit
+//
+//  Created by Kyle Robson on 1/15/13.
+//  Copyright (c) 2013 Robson Software Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+CGRect CGRectChop (CGRect rect, CGFloat amount, CGRectEdge edge);
+CGRect CGRectGrow (CGRect rect, CGFloat amount, CGRectEdge edge);
+CGRect CGRectWithSize (CGSize size);
+CGPoint CGPointFloor(CGPoint point);
+CGPoint CGRectCenterPoint (CGRect rect);
+CGRect CGRectCenterInRect(CGRect containerRect, CGRect containedRect);
+CGRect CGRectCenterVerticallyInRect(CGRect containerRect, CGRect containedRect, CGFloat originX);
+CGRect CGRectCenterHorizontallyInRect(CGRect containerRect, CGRect containedRect, CGFloat originY);
---
+++ KRKit/CGGeometry+KRAdditions.m
@@ -0,0 +1,78 @@
+//
+//  CGGeometry+KRAdditions.m
+//  KRKit
+//
+//  Created by Kyle Robson on 1/15/13.
+//  Copyright (c) 2013 Robson Software Consulting. All rights reserved.
+//
+
+#import "CGGeometry+KRAdditions.h"
+
+CGRect CGRectChop(CGRect rect, CGFloat amount, CGRectEdge edge)
+{
+    CGRect slice, remainder;
+    CGRectDivide(rect, &slice, &remainder, amount, edge);
+    
+    return remainder;
+}
+
+CGRect CGRectGrow(CGRect rect, CGFloat amount, CGRectEdge edge)
+{
+    switch (edge) {
+        case CGRectMinXEdge:
+        {
+            return CGRectMake(CGRectGetMinX(rect) - amount, CGRectGetMinY(rect), CGRectGetWidth(rect) + amount, CGRectGetHeight(rect));
+        }
+        case CGRectMinYEdge:
+        {
+            return CGRectMake(CGRectGetMinX(rect), CGRectGetMinY(rect) - amount, CGRectGetWidth(rect), CGRectGetHeight(rect) + amount);
+        }
+        case CGRectMaxXEdge:
+        {
+            return CGRectMake(CGRectGetMinX(rect), CGRectGetMinY(rect), CGRectGetWidth(rect) + amount, CGRectGetHeight(rect));
+        }
+        case CGRectMaxYEdge:
+        {
+            return CGRectMake(CGRectGetMinX(rect), CGRectGetMinY(rect), CGRectGetWidth(rect), CGRectGetHeight(rect) + amount);
+        }
+        default:
+        {
+            NSCAssert(NO, @"Unrecognized CGRectEdge %i", (int)edge);
+            return CGRectZero;
+        }
+    }
+}
+
+CGRect CGRectWithSize(CGSize size)
+{
+    return (CGRect) { CGPointZero, size };
+}
+
+CGPoint CGPointFloor(CGPoint point)
+{
+    return CGPointMake(floorf(point.x), ceilf(point.y));
+}
+
+CGPoint CGRectCenterPoint(CGRect rect)
+{
+    return CGPointMake(CGRectGetMinX(rect) + (CGRectGetWidth(rect) / 2), CGRectGetMinY(rect) + (CGRectGetHeight(rect) / 2));
+}
+
+CGRect CGRectCenterInRect(CGRect containerRect, CGRect containedRect)
+{
+    return (CGRect) { CGRectCenterPoint(containedRect), containedRect.size };
+}
+
+CGRect CGRectCenterHorizontallyInRect(CGRect containerRect, CGRect containedRect, CGFloat originY)
+{
+    CGRect rect = (CGRect) { CGRectCenterPoint(containedRect), containedRect.size };
+    rect.origin.y = originY;
+    return rect;
+}
+
+CGRect CGRectCenterVerticallyInRect(CGRect containerRect, CGRect containedRect, CGFloat originX)
+{
+    CGRect rect = (CGRect) { CGRectCenterPoint(containedRect), containedRect.size };
+    rect.origin.x = originX;
+    return rect;
+}

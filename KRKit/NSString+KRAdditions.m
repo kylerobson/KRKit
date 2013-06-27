5a7472e70b5cc486f30f7f8bf8453a7789cf0e55
= <=>
2013-06-27 13:03:11 -0500

    .

--- KRKit/NSString+KRAdditions.m
+++ KRKit/NSString+KRAdditions.m
@@ -214,14 +214,14 @@
 - (CGFloat)fontSizeWithFont:(UIFont *)font constrainedToSize:(CGSize)size minimumFontSize:(CGFloat)minimumFontSize
 {
     CGFloat fontSize = [font pointSize];
-    CGFloat height = [self sizeWithFont:font constrainedToSize:CGSizeMake(size.width,FLT_MAX) lineBreakMode:UILineBreakModeWordWrap].height;
+    CGFloat height = [self sizeWithFont:font constrainedToSize:CGSizeMake(size.width,FLT_MAX) lineBreakMode:NSLineBreakByWordWrapping].height;
     UIFont *newFont = font;
     
     //Reduce font size while too large, break if no height (empty string)
     while (height > size.height && height != 0 && fontSize > minimumFontSize) {
         fontSize--;
         newFont = [UIFont fontWithName:font.fontName size:fontSize];
-        height = [self sizeWithFont:newFont constrainedToSize:CGSizeMake(size.width,FLT_MAX) lineBreakMode:UILineBreakModeWordWrap].height;
+        height = [self sizeWithFont:newFont constrainedToSize:CGSizeMake(size.width,FLT_MAX) lineBreakMode:NSLineBreakByWordWrapping].height;
     };
     
     // Loop through words in string and resize to fit
